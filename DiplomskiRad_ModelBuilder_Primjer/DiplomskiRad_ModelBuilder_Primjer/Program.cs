2baaf52f97f0dd0c33377961e0c85d9c2d9883e8
Kristijan Maoduš <kmaodus@foi.hr>
2021-08-29 20:34:46 +0200

    Example done

--- DiplomskiRad_ModelBuilder_Primjer/DiplomskiRad_ModelBuilder_Primjer/Program.cs
+++ DiplomskiRad_ModelBuilder_Primjer/DiplomskiRad_ModelBuilder_Primjer/Program.cs
@@ -6,7 +6,23 @@ namespace DiplomskiRad_ModelBuilder_Primjer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("--- Testiranje na primjeru ---\n");
+
+
+            //Učitavanje primjera
+            var sampleData = new TitanicModel.ModelInput()
+            {
+                Sex = @"male",
+                Age = 24F,
+            };
+
+            //Učitavanje modela i predviđanje statusa osobe
+            var result = TitanicModel.Predict(sampleData);
+
+            string status = result.Prediction == 1 ? "živ" : "mrtav";
+            Console.WriteLine($"Model predviđa da je status {(sampleData.Sex.Equals("male") ? "muške" : "ženske")} osobe nakon nesreće na Titanicu: {status}");
+
+            Console.ReadKey();
         }
     }
 }
