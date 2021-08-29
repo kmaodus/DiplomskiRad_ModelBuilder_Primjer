b4f27516e451dded8efbe92768be4dce53631dec
Kristijan Maoduš <kmaodus@foi.hr>
2021-08-29 18:17:11 +0200

    Console app project created

---
+++ DiplomskiRad_ModelBuilder_Primjer/DiplomskiRad_ModelBuilder_Primjer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiplomskiRad_ModelBuilder_Primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
